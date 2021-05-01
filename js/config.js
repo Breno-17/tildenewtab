b8b5f7cd3462ea52fe663bf358200a6f12a968b5
Breno-17 <65715431+Breno-17@users.noreply.github.com>
2021-05-01 13:42:56 +0930

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,239 @@
+const CONFIG = {
+  /**
+   * The category, name, key, url, search path, color, icon, and quicklaunch properties for your commands.
+   * Icons must be added to "icons" folder and their values/names must be updated.
+   * If none of the specified keys are matched, the '*' key is used.
+   * Commands without a category don't show up in the help menu.
+   * Update line 11 and 13 if you prefer using Google.
+   */
+  commands: [{
+      name: 'Google',
+      key: '*',
+      url: 'https://google.com',
+      search: '/search?q={}'
+    },
+	{
+      category: 'General',
+      name: 'Outlook',
+      key: 'out',
+      url: 'https://outlook.office.com/mail/inbox',
+      color: '#5682a3',
+      icon: 'mail',
+      quickLaunch: false,
+    },
+    {
+      category: 'General',
+      name: 'Gmail',
+      key: 'gm',
+      url: 'https://gmail.com',
+      search: '/#search/text={}',
+      color: 'linear-gradient(135deg, #dd5145, #dd5145)',
+      icon: 'mail',
+      quickLaunch: true,
+    },
+    {
+      category: 'General',
+      name: 'Drive',
+      key: 'dr',
+      url: 'https://drive.google.com',
+      search: '/drive/search?q={}',
+      color: 'linear-gradient(135deg, #FFD04B, #1EA362, #4688F3)',
+      icon: 'drive',
+      quickLaunch: false,
+    },
+    
+	
+    
+    
+{
+      category: 'Social',
+      name: 'YouTube',
+      key: 'yt',
+      url: 'https://youtube.com',
+      search: '/results?search_query={}',
+      color: 'linear-gradient(135deg, #cd201f, #cd4c1f)',
+      icon: 'youtube',
+      quickLaunch: false,
+    },
+    {
+      category: 'Social',
+      name: 'Reddit',
+      key: 'r',
+      url: 'https://reddit.com',
+      search: '/search?q={}',
+      color: 'linear-gradient(135deg, #FF8456, #FF4500)',
+      icon: 'reddit',
+      quickLaunch: false,
+    },
+	
+        
+	{
+      category: 'Social',
+      name: 'Discord',
+      key: 'dis',
+      url: 'https://discord.com/app',
+      color: '#7289da',
+      icon: 'discord',
+      quickLaunch: false,
+    },
+	{
+      category: 'Investing',
+      name: 'Coinspot',
+      key: 'coin',
+      url: 'https://www.coinspot.com.au/my/dashboard',
+      search: '/search?stories[query]={}',
+      color: 'linear-gradient(135deg, #FF6600, #DC5901)',
+      icon: 'telegram',
+      quickLaunch: true,
+    },
+    {
+      category: 'Investing',
+      name: 'Octopus Group',
+      key: 'oct',
+      url: 'https://my.octopusgroup.com.au/',
+      search: '/search?q={}',
+      color: 'linear-gradient(135deg, #53341C, #F48024)',
+      icon: 'seslisozluk',
+      quickLaunch: true,
+    },
+    {
+      category: 'Investing',
+      name: 'Toluna',
+      key: 'tol',
+      url: 'https://au.toluna.com/SurveyCenter/surveys',
+      search: '/search?q={}',
+      color: 'linear-gradient(135deg, #2b2b2b, #3b3b3b)',
+      icon: 'eksisozluk',
+      quickLaunch: true,
+    },
+     
+
+	{
+      category: 'Other',
+      name: 'Instagram',
+      key: 'i',
+      url: 'https://www.instagram.com',
+      color: 'linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d)',
+      icon: 'instagram',
+      quickLaunch: true,
+    },
+	{
+      category: 'Other',
+      name: 'Spotify',
+      key: 's',
+      url: 'https://open.spotify.com/',
+      search: '/search/{}',
+      color: '#1dd35e',
+      icon: 'spotify',
+      quickLaunch: false,
+    },
+	{
+      category: 'Other',
+      name: 'Translate',
+      key: 'tr',
+      url: 'https://translate.google.com/',
+      search: '/#view=home&op=translate&sl=auto&tl=en&text={}',
+      color: '#1a73e8',
+      icon: 'translate',
+      quickLaunch: false,
+    },	
+    
+    
+    
+    
+  ],
+
+  /**
+   * Get suggestions as you type.
+   */
+  suggestions: true,
+  suggestionsLimit: 4,
+
+  /**
+   * The order and limit for each suggestion influencer. An "influencer" is
+   * just a suggestion source. The following influencers are available:
+   *
+   * - "Commands" suggestions come from CONFIG.commands
+   * - "Default" suggestions come from CONFIG.defaultSuggestions
+   * - "DuckDuckGo" suggestions come from the duck duck go search api
+   * - "History" suggestions come from your previously entered queries
+   */
+  influencers: [{
+      name: 'Commands',
+      limit: 2
+    },
+    {
+      name: 'Default',
+      limit: 4
+    },
+    {
+      name: 'History',
+      limit: 1
+    },
+    {
+      name: 'DuckDuckGo',
+      limit: 4
+    },
+  ],
+
+  /**
+   * Default search suggestions for the specified queries.
+   */
+  defaultSuggestions: {
+    g: ['g/issues', 'g/pulls', 'gist.github.com'],
+    r: ['r/r/unixporn', 'r/r/startpages', 'r/r/webdev', 'r/r/technology'],
+  },
+
+  /**
+   * Instantly redirect when a key is matched. Put a space before any other
+   * queries to prevent unwanted redirects.
+   */
+  instantRedirect: false,
+
+  /**
+   * Open triggered queries in a new tab.
+   */
+  newTab: false,
+
+  /**
+   * Dynamic overlay background colors when command domains are matched.
+   */
+  colors: true,
+
+  /**
+   * Invert color theme
+   */
+  invertedColors: false,
+
+  /**
+   * Show keys instead of icons
+   */
+  showKeys: false,
+
+  /**
+   * The delimiter between a command key and your search query. For example,
+   * to search GitHub for potatoes, you'd type "g:potatoes".
+   */
+  searchDelimiter: ':',
+
+  /**
+   * The delimiter between a command key and a path. For example, you'd type
+   * "r/r/unixporn" to go to "https://reddit.com/r/unixporn".
+   */
+  pathDelimiter: '/',
+
+  /**
+   * The delimiter between the hours and minutes on the clock.
+   */
+  clockDelimiter: ' ',
+
+  /**
+   * Show a twenty-four-hour clock instead of a twelve-hour clock with AM/PM.
+   */
+  twentyFourHourClock: true,
+
+  /**
+   * File extension for icon images
+   */
+  iconExtension: 'png'
+};
